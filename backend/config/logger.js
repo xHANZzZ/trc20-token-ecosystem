d19c62d7a7b8c351035a5eb8422e9ba7484fad3e
xHANZzZ <nadirshahff2020@gmail.com>
2026-06-11 23:34:50 +0500

    feat: add backend/config/logger.js

---
+++ backend/config/logger.js
@@ -0,0 +1,26 @@
+import winston from 'winston';
+
+const { combine, timestamp, printf, colorize, errors } = winston.format;
+
+// Custom text format representation
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+  return `${timestamp} [${level}]: ${stack || message}`;
+});
+
+export const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+  format: combine(
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    errors({ stack: true }), // Include stack trace if error exists
+    logFormat
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: combine(
+        colorize(),
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        logFormat
+      )
+    })
+  ]
+});
