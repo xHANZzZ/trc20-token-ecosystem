475fb36b6e6d6fa175b605a21c558973d46fdccc
xHANZzZ <nadirshahff2020@gmail.com>
2026-06-11 23:40:03 +0500

    fix: update relative imports in frontend/src/pages/Transfers.tsx

--- frontend/src/pages/Transfers.tsx
+++ frontend/src/pages/Transfers.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Button } from '../components/Button.js';
-import { Input } from '../components/Input.js';
-import { useTronLink } from '../hooks/useTronLink.js';
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
+import { useTronLink } from '../hooks/useTronLink';
 
 interface TransactionRecord {
   txid: string;
